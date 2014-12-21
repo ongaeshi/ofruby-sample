576b2a5b045f511681f121037f08740b82ad8224
ongaeshi <ongaeshi0621@gmail.com>
2014-12-22 02:00:44 +0900

    Tweak the player speed. Add Obstacle

--- space_jumper.rb
+++ space_jumper.rb
@@ -3,6 +3,15 @@ def setup
   
   @player = Player.new
   @updater << @player
+
+  @updater << Obstacle.new
+  @updater << Obstacle.new
+  @updater << Obstacle.new
+  @updater << Obstacle.new
+  @updater << Obstacle.new
+  @updater << Obstacle.new
+  @updater << Obstacle.new
+  @updater << Obstacle.new
 end
 
 def update
@@ -14,7 +23,8 @@ def draw
 end
 
 class Player
-  ACCEL_Y = 0.04
+  ACCEL_Y = 0.04 * 14
+  SPEED_Y = -10
 
   def initialize
     @x = 100
@@ -23,8 +33,8 @@ class Player
   end
   
   def update
-    touched = Input.touches.find_all { |e| e.press? }
-    @speed_y += -0.1 if touched
+    touched = Input.touches.find { |e| e.press? }
+    @speed_y = SPEED_Y if touched
 
     @speed_y += ACCEL_Y
     
@@ -34,5 +44,23 @@ class Player
   def draw
     set_color_hex 0x4B8CF6
     rect @x - 5, @y - 5, 10, 10
+  end
+end
+
+class Obstacle
+  def initialize
+    @x = width + 100
+    @y = rand(height)
+    @speed_x = -rand(10)
+    @radius = rand(50)
+  end
+  
+  def update
+    @x += @speed_x
+  end
+  
+  def draw
+    set_color_hex 0xF5F5F5
+    circle @x, @y, @radius
   end
 end
