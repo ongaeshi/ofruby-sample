c2c132248d11f632f4dd0cb73aeba7bc7ad08ff8
ongaeshi <ongaeshi0621@gmail.com>
2015-02-22 14:11:15 +0900

    Update sample

--- stripe.rb
+++ stripe.rb
@@ -1,15 +1,34 @@
-WIDTH = 320
-HEIGHT = 480
 SIZE = 20
+ROTATE = 30
+COLOR1 = 0xC9F8F1
+COLOR2 = 0xCA3C6E
+
+def setup
+  @size = SIZE
+  @color1 = COLOR1
+  @color2 = COLOR2
+end
 
 def draw
   set_background_hex 0xffffff
   set_fill
 
-  0.step HEIGHT, SIZE*2 do |i|
-    set_color_hex 0xC9F8F1
-    rect 0, i, WIDTH, SIZE
-    set_color_hex 0xCA3C6E
-    rect i, 0, SIZE, HEIGHT
+  if Input.touch(0).valid? && Input.touch(1).valid?
+    @size = SIZE
+    @color1 = COLOR1
+    @color2 = COLOR2
+  elsif Input.touch(0).press?
+    @size = SIZE + rand(SIZE) - 5
+    @color1 = rand(0xffffff)
+    @color2 = rand(0xffffff)
+  end
+
+  rotate ROTATE
+
+  0.step height, @size*2 do |i|
+    set_color_hex @color1
+    rect 0, i, width, @size
+    set_color_hex @color2
+    rect i, 0, @size, height
   end
 end
